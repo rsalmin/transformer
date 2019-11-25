39877873281f007613fa95cb9d981df6622a54ff
Roman Salmin <Roman.Salmin@gmail.com>
2019-11-25 21:50:54 +0300

    separate module for positional encoding

--- transformer.py
+++ transformer.py
@@ -16,55 +16,11 @@ import tensorflow as tf
 
 import inputData
 from prepareDatasets import train_dataset, val_dataset, BATCH_SIZE, input_vocab_size, target_vocab_size
+from posEncoding import positional_encoding
 
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-
-#BUFFER_SIZE = 20000
-#MAX_LENGTH = 40
-
-"""## Positional encoding
-
-Since this model doesn't contain any recurrence or convolution, positional encoding is added to give the model some information about the relative position of the words in the sentence.
-
-The positional encoding vector is added to the embedding vector. Embeddings represent a token in a d-dimensional space where tokens with similar meaning will be closer to each other. But the embeddings do not encode the relative position of words in a sentence. So after adding the positional encoding, words will be closer to each other based on the *similarity of their meaning and their position in the sentence*, in the d-dimensional space.
-
-See the notebook on [positional encoding](https://github.com/tensorflow/examples/blob/master/community/en/position_encoding.ipynb) to learn more about it. The formula for calculating the positional encoding is as follows:
-
-$$\Large{PE_{(pos, 2i)} = sin(pos / 10000^{2i / d_{model}})} $$
-$$\Large{PE_{(pos, 2i+1)} = cos(pos / 10000^{2i / d_{model}})} $$
-"""
-
-def get_angles(pos, i, d_model):
-  angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
-  return pos * angle_rates
-
-def positional_encoding(position, d_model):
-  angle_rads = get_angles(np.arange(position)[:, np.newaxis],
-                          np.arange(d_model)[np.newaxis, :],
-                          d_model)
-
-  # apply sin to even indices in the array; 2i
-  angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-
-  # apply cos to odd indices in the array; 2i+1
-  angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-
-  pos_encoding = angle_rads[np.newaxis, ...]
-
-  return tf.cast(pos_encoding, dtype=tf.float32)
-
-pos_encoding = positional_encoding(50, 512)
-print (pos_encoding.shape)
-
-plt.pcolormesh(pos_encoding[0], cmap='RdBu')
-plt.xlabel('Depth')
-plt.xlim((0, 512))
-plt.ylabel('Position')
-plt.colorbar()
-plt.show()
 
 """## Masking
 
